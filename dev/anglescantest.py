e03bce11be5211ad742b9cb26e347351382875d0
lmcfadd6 <lmcfadd6@uwo.ca>
2021-01-05 14:35:44 -0500

    file reorg

---
+++ dev/anglescantest.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from supra.Supracenter.anglescan import anglescan
+
+sounding = np.array([[10000, 300,  90, 40], 
+					 [ 9000, 300,  90, 40], 
+					 [ 8000, 300,  90, 40], 
+					 [ 7000, 300,  90, 40], 
+					 [ 6000, 300,  90, 40], 
+					 [ 5000, 300,  90, 40], 
+					 [ 4000, 300,  90, 40], 
+					 [ 3000, 300, 270, 40], 
+					 [ 2000, 300, 180, 40], 
+					 [    0, 300,  90, 40]])
+
+S = np.array([0, 0, 10000])
+
+theta = 135
+phi = 45
+
+results = anglescan(S, phi, theta, sounding, wind=True, h_tol=330, v_tol=1000)
+print(results)
